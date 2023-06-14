bcca68cecf6e33a967a98240de6697228ba202ef
DJsynther42 <128554237+DJsynther42@users.noreply.github.com>
2023-06-13 19:18:34 -0500

    Update WineListAppApplication.java

--- Wine-list-app/src/main/java/com/example/demo/WineListAppApplication.java
+++ Wine-list-app/src/main/java/com/example/demo/WineListAppApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WineListAppApplication {
+	
+	/**
+	 * @author David Burleson
+	 * main is the main method
+	 * @param args String array 
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(WineListAppApplication.class, args);
